c7ec62321470049c073ee625970a695784358fe9
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2021-03-04 21:49:20 -0300

    Document image functions and change a signature
    Change image_set signature
    
    Document the image functions

--- image.h
+++ image.h
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include "defs.h"
 
+/**
+ * Struct that stores a RGB color 
+ */
 typedef struct Color {
     u8 red;
     u8 green;
@@ -11,7 +14,8 @@ typedef struct Color {
 } Color;
 
 /**
- * Manipulate a image
+ * Strcut that stores a image
+ * and allow to manipulate it
  */
 typedef struct Image {
     i32 width;
@@ -19,14 +23,30 @@ typedef struct Image {
     Color* pixels;
 } Image;
 
+/**
+ * Allocate image struct
+ */
 Image* image_new(i32 width, i32 height);
+
+/**
+ * Delete image from memory and
+ * free struct from the memory
+ */
 void image_del(Image* restrict image);
 
-inline static void image_set(Image* restrict image, int x, int y, Color color) {
+/**
+ * Set the value of pixel at (x, y) to color
+ */
+inline static void image_set(Image* restrict image, i32 x, i32 y, Color color) {
     image->pixels[y * image->height + x] = color;
 }
 
+/**
+ * Set all pixels of image to color
+ */
 void image_clear(Image* restrict image, Color color);
 
-// Save the image as a bitmap file
+/**
+ * Save the image as a bitmap file
+ */
 bool image_save(Image* restrict image, char* path);
