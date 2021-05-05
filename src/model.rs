6be40af6b056c1c9100c46abec0ad55c5e33ace9
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2021-05-05 18:26:31 -0300

    Modfy model to read vn from Wavefront obj
    And changing some error messages in model

--- src/model.rs
+++ src/model.rs
@@ -4,10 +4,11 @@ use std::io::Read;
 use crate::image::Image;
 use crate::modules::{Color, Matrix, Vertex2, Vertex3};
 
-type Element = (isize, Option<isize>);
+type Element = (isize, Option<isize>, Option<isize>);
 pub struct Model {
     vertices: Vec<Vertex3>,
     textures: Vec<Vertex2>,
+    normal: Vec<Vertex3>,
     faces: Vec<Vec<Element>>,
 }
 
@@ -19,7 +20,7 @@ impl Model {
                 Some(v) => v.0,
                 None => continue,
             };
-            for (u, _) in face {
+            for (u, _, _) in face {
                 image.line(
                     u.to_point(image.width, image.height),
                     v.to_point(image.width, image.height),
@@ -36,9 +37,9 @@ impl Model {
         let mut zbuffer: Vec<f64> = vec![f64::NEG_INFINITY; (image.width * image.height) as usize];
 
         for face in self.faces() {
-            let (u, _) = face[0];
-            let (v, _) = face[1];
-            let (w, _) = face[2];
+            let (u, _, _) = face[0];
+            let (v, _, _) = face[1];
+            let (w, _, _) = face[2];
 
             let normal = (w - u).cross(v - u).normalize();
             let intensity = normal * light;
@@ -54,13 +55,13 @@ impl Model {
         let mut zbuffer: Vec<f64> = vec![f64::NEG_INFINITY; (image.width * image.height) as usize];
 
         for face in self.faces() {
-            let (u, ut) = face[0];
-            let (v, vt) = face[1];
-            let (w, wt) = face[2];
+            let (u, ut, _) = face[0];
+            let (v, vt, _) = face[1];
+            let (w, wt, _) = face[2];
 
-            let ut = ut.expect("Render Texture: No texture vertex");
-            let vt = vt.expect("Render Texture: No texture vertex");
-            let wt = wt.expect("Render Texture: No texture vertex");
+            let ut = ut.expect("Model have no texture vertex");
+            let vt = vt.expect("Model have no texture vertex");
+            let wt = wt.expect("Model have no texture vertex");
 
             let normal = (w - u).cross(v - u).normalize();
             let intensity = normal * light;
@@ -72,6 +73,24 @@ impl Model {
                 (u, v, w),
                 (ut, vt, wt),
             );
+        }
+    }
+
+    fn compute_normal(&mut self) {
+        // TODO
+        self.normal.push(Vertex3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        });
+
+        for vec in &mut self.faces {
+            for i in 0..vec.len() {
+                let (v, vt, vn) = vec[i];
+                if vn.is_none() {
+                    vec[i] = (v, vt, Some(self.normal.len() as isize));
+                }
+            }
         }
     }
 
@@ -112,13 +131,13 @@ impl Model {
         let mut zbuffer: Vec<f64> = vec![f64::NEG_INFINITY; (image.width * image.height) as usize];
 
         for face in self.faces() {
-            let (u, ut) = face[0];
-            let (v, vt) = face[1];
-            let (w, wt) = face[2];
+            let (u, ut, _) = face[0];
+            let (v, vt, _) = face[1];
+            let (w, wt, _) = face[2];
 
-            let ut = ut.expect("Render Texture: No texture vertex");
-            let vt = vt.expect("Render Texture: No texture vertex");
-            let wt = wt.expect("Render Texture: No texture vertex");
+            let ut = ut.expect("Model have no texture vertex");
+            let vt = vt.expect("Model have no texture vertex");
+            let wt = wt.expect("Model have no texture vertex");
 
             let up = matrix_to_vertex3(&transform * &vertex3_to_matrix(u));
             let vp = matrix_to_vertex3(&transform * &vertex3_to_matrix(v));
@@ -148,8 +167,10 @@ impl Model {
             vertices: Vec::new(),
             faces: Vec::new(),
             textures: Vec::new(),
+            normal: Vec::new(),
         };
 
+        let mut no_computed_normal = false;
         for line in lines {
             let mut data = line.split(" ").filter(|string| !string.is_empty());
 
@@ -157,50 +178,68 @@ impl Model {
                 Some("v") => model.vertices.push(Vertex3 {
                     x: data
                         .next()
-                        .expect("Invalid Wavefront Obj: Vertex must have x coordinate")
+                        .expect("Invalid Wavefront Obj: Vertex doesn't have x coordinate")
                         .trim()
                         .parse::<f64>()
-                        .expect("Invalid Wavefront Obj: Vertex coordinate must be a float"),
+                        .expect("Invalid Wavefront Obj: Vertex x coordinate isn't a float"),
                     y: data
                         .next()
-                        .expect("Invalid Wavefront Obj: Vertex must have y coordinate")
+                        .expect("Invalid Wavefront Obj: Vertex doesn't have y coordinate")
                         .trim()
                         .parse::<f64>()
-                        .expect("Invalid Wavefront Obj: Vertex coordinate must be a float"),
+                        .expect("Invalid Wavefront Obj: Vertex y coordinate isn't float"),
                     z: data
                         .next()
-                        .expect("Invalid Wavefront Obj: Vertex must have z coordinate")
+                        .expect("Invalid Wavefront Obj: Vertex doesn't have z coordinate")
                         .trim()
                         .parse::<f64>()
-                        .expect("Invalid Wavefront Obj: Vertex coordinate must be a float"),
+                        .expect("Invalid Wavefront Obj: Vertex z coordinate isn't float"),
                 }),
                 Some("vt") => model.textures.push(Vertex2 {
                     x: data
                         .next()
-                        .expect("Invalid Wavefront Obj: Texture Vertex must have x coordinate")
+                        .expect("Invalid Wavefront Obj: Texture Vertex doesn't have x coordinate")
                         .trim()
                         .parse::<f64>()
-                        .expect("Invalid Wavefront Obj: Texture Vertex coordinate must be a float"),
+                        .expect("Invalid Wavefront Obj: Texture Vertex x coordinate isn't a float"),
                     y: data
                         .next()
-                        .expect("Invalid Wavefront Obj: Texture Vertex must have y coordinate")
+                        .expect("Invalid Wavefront Obj: Texture Vertex doesn't have y coordinate")
                         .trim()
                         .parse::<f64>()
-                        .expect("Invalid Wavefront Obj: Texture Vertex coordinate must be a float"),
+                        .expect("Invalid Wavefront Obj: Texture Vertex y coordinate isn't a float"),
+                }),
+                Some("vn") => model.normal.push(Vertex3 {
+                    x: data
+                        .next()
+                        .expect("Invalid Wavefront Obj: Normal doesn't have x coordinate")
+                        .trim()
+                        .parse::<f64>()
+                        .expect("Invalid Wavefront Obj: Normal x coordinate isn't a float"),
+                    y: data
+                        .next()
+                        .expect("Invalid Wavefront Obj: Normal doesn't have y coordinate")
+                        .trim()
+                        .parse::<f64>()
+                        .expect("Invalid Wavefront Obj: Normal y coordinate isn't a float"),
+                    z: data
+                        .next()
+                        .expect("Invalid Wavefront Obj: Normal doesn't have z coordinate")
+                        .trim()
+                        .parse::<f64>()
+                        .expect("Invalid Wavefront Obj: Normal z coordinate isn't a float"),
                 }),
                 Some("f") => {
                     let mut face: Vec<Element> = Vec::new();
                     for element in data {
-                        let mut element = element.split("/");
+                        let mut element = element.split("/").filter(|string| !string.is_empty());
                         let vertex_index = element
                             .next()
-                            .expect(
-                                "Invalid Wavefront Obj: The Face's vertex index must be a integer",
-                            )
+                            .expect("Invalid Wavefront Obj: no face vertex index")
                             .trim()
                             .parse::<isize>()
                             .expect(
-                                "Invalid Wavefront Obj: The Face's vertex index must be a integer",
+                                "Invalid Wavefront Obj: The face vertex index isn't an integer",
                             );
 
                         let texture_index = match element.next() {
@@ -209,18 +248,39 @@ impl Model {
                                     .trim()
                                     .parse::<isize>()
                                     .expect(
-                                        "Invalid Wavefront Obj: The Face's texture vertex index must be a integer"
+                                        "Invalid Wavefront Obj: The face texture vertex index isn't an integer"
                                     )
                                 )
                             }
                             None => None,
                         };
-                        face.push((vertex_index, texture_index));
+
+                        let normal_index = match element.next() {
+                            Some(string) => {
+                                Some(string
+                                    .trim()
+                                    .parse::<isize>()
+                                    .expect(
+                                        "Invalid Wavefront Obj: The face normal vertex index isn't an integer"
+                                    )
+                                )
+                            }
+                            None => {
+                                no_computed_normal = true;
+                                None
+                            },
+                        };
+
+                        face.push((vertex_index, texture_index, normal_index));
                     }
                     model.faces.push(face);
                 }
                 _ => continue,
             }
+        }
+
+        if no_computed_normal {
+            model.compute_normal();
         }
 
         Ok(model)
@@ -240,7 +300,7 @@ pub struct FaceIterator<'a> {
 }
 
 impl<'a> Iterator for FaceIterator<'a> {
-    type Item = Vec<(Vertex3, Option<Vertex2>)>;
+    type Item = Vec<(Vertex3, Option<Vertex2>, Vertex3)>;
     fn next(&mut self) -> Option<Self::Item> {
         if self.index >= self.model.faces.len() {
             return None;
@@ -251,14 +311,17 @@ impl<'a> Iterator for FaceIterator<'a> {
         let mut result: Self::Item = Vec::new();
 
         for element in face {
-            let &(vertex_index, texture_index) = element;
+            // Vertex
+            let &(vertex_index, texture_index, normal_index) = element;
+
             let vertex_index = if vertex_index >= 0 {
                 vertex_index - 1
             } else {
                 model.vertices.len() as isize + vertex_index
-            } as usize;
-            let vertex = self.model.vertices[vertex_index];
+            };
+            let vertex = self.model.vertices[vertex_index as usize];
 
+            // Texture
             let texture_index = match texture_index {
                 Some(texture_index) => {
                     if texture_index >= 0 {
@@ -274,7 +337,20 @@ impl<'a> Iterator for FaceIterator<'a> {
                 None => None,
             };
 
-            result.push((vertex, texture));
+            // Normal
+            let normal_index = match normal_index {
+                Some(normal_index) => {
+                    if normal_index >= 0 {
+                        normal_index - 1
+                    } else {
+                        model.textures.len() as isize + normal_index
+                    }
+                }
+                None => panic!("Normal not computed"),
+            };
+            let normal = model.normal[normal_index as usize];
+
+            result.push((vertex, texture, normal));
         }
 
         self.index += 1;
